7f966dcabc871cf25890418eea9443496358f827
Siam <sahm0036@student.monash.edu>
2024-09-04 17:35:23 +1000

    mongodb

--- part3/index.js
+++ part3/index.js
@@ -1,5 +1,7 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
+const Note = require('./models/note')
 
 app.use(express.json())
 const cors = require('cors')
@@ -29,21 +31,18 @@ let notes = [
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
   })
+  
 
   app.get('/api/notes', (request, response) => {
-    response.json(notes)
+    Note.find({}).then(notes => {
+      response.json(notes)
+    })
   })
-  
+
   app.get('/api/notes/:id', (request, response) => {
-    const id = request.params.id
-    const note = notes.find(note => note.id === id)
-    
-  
-    if (note) {
+    Note.findById(request.params.id).then(note => {
       response.json(note)
-    } else {
-      response.status(404).end()
-    }
+    })
   })
 
   app.delete('/api/notes/:id', (request, response) => {
@@ -63,24 +62,21 @@ app.get('/', (request, response) => {
   app.post('/api/notes', (request, response) => {
     const body = request.body
   
-    if (!body.content) {
-      return response.status(400).json({ 
-        error: 'content missing' 
-      })
+    if (body.content === undefined) {
+      return response.status(400).json({ error: 'content missing' })
     }
   
-    const note = {
+    const note = new Note({
       content: body.content,
-      important: Boolean(body.important) || false,
-      id: generateId(),
-    }
+      important: body.important || false,
+    })
   
-    notes = notes.concat(note)
-  
-    response.json(note)
+    note.save().then(savedNote => {
+      response.json(savedNote)
+    })
   })
   
-  const PORT = process.env.PORT || 3001
+  const PORT = process.env.PORT
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
